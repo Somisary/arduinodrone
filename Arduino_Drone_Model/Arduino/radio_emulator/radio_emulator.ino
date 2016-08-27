a7a77e155580ad4baa1aaad29b29e00dee79731a
Somisary <somisary@gmail.com>
2016-08-27 16:50:48 +0100

    MATBAR: Proof-of-concept send data Arduino to Unity

--- Arduino_Drone_Model/Arduino/radio_emulator/radio_emulator.ino
+++ Arduino_Drone_Model/Arduino/radio_emulator/radio_emulator.ino
@@ -1,9 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(8);
+  delay(100);        // delay in between reads for stability
 }
